f5fda0e558475fc8a8e21d80f2f7adcc7c8f606c
Eugene Kapelko <softwarearchitect19@gmail.com>
2024-08-01 22:17:01 +0300

    Update project. Add factory.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,6 +16,11 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
+	mux.Get("/test-patterns", app.TestPatterns)
+
+	mux.Get("/api/dog-from-factory", app.CreateDogFromFactory)
+	mux.Get("/api/cat-from-factory", app.CreateCatFromFactory)
+
 	mux.Get("/", app.ShowHome)
 	mux.Get("/{page}", app.ShowPage)
 	return mux
