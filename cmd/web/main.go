5ba961dc102aeece0bf8225fc808c060d7086473
Eugene Kapelko <softwarearchitect19@gmail.com>
2024-08-13 18:06:16 +0300

    Refactoring.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"breeders/adapters"
 	"breeders/configuration"
+	"breeders/streamer"
 	"flag"
 	"fmt"
 	"html/template"
@@ -17,6 +18,7 @@ type application struct {
 	templateMap map[string]*template.Template
 	config      appConfig
 	App         *configuration.Application
+	videoQueue  chan streamer.VideoProcessingJob
 }
 
 type appConfig struct {
@@ -25,8 +27,13 @@ type appConfig struct {
 }
 
 func main() {
+	const numWorkers = 4
+	videoQueue := make(chan streamer.VideoProcessingJob, numWorkers)
+	defer close(videoQueue)
+
 	app := &application{
 		templateMap: make(map[string]*template.Template),
+		videoQueue:  videoQueue,
 	}
 
 	flag.BoolVar(&app.config.useCache, "cache", false, "use cache for templates")
@@ -45,6 +52,9 @@ func main() {
 	xmlAdapter := &adapters.RemoteService{Remote: xmlBackend}
 
 	app.App = configuration.New(db, xmlAdapter)
+
+	wp := streamer.New(videoQueue, numWorkers)
+	wp.Run()
 
 	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	//	fmt.Fprint(w, "Hello, World!")
